e5c3080a9084e4771c4d1cd288e8b967804f0bc4
soso <soumiaelhali@gmail.com>
2020-02-10 15:13:31 +0100

    after route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,22 @@ import { Routes, RouterModule } from '@angular/router';
 import { BlogComponent} from './blog/blog.component';
 import {HomeComponent} from './home/home.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { VenteComponent } from './vente/vente.component';
+import { LocationComponent } from './location/location.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { PublierAnnonceComponent } from './publier-annonce/publier-annonce.component';
+import { ConnexionComponent } from './connexion/connexion.component';
 
 
 const routes: Routes = [
   {path :'blog', component: BlogComponent},
   {path : 'home', component: HomeComponent},
-  {path :'', redirectTo: '/home', pathMatch: 'full'}
+  {path : 'achat' , component : VenteComponent},
+  {path : 'location' , component : LocationComponent},
+  {path : 'contact-us' , component : ContactUsComponent},
+  {path : 'publierannonce', component: PublierAnnonceComponent},
+  {path : 'connexion' , component : ConnexionComponent},
+  {path : '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
 @NgModule({
