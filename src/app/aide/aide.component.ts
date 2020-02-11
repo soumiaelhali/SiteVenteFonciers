aa7ee6ba2d5a7bf3cc43fec962c93d1cccb679f1
soso <soumiaelhali@gmail.com>
2020-02-11 13:51:50 +0100

    update conx

---
+++ src/app/aide/aide.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aide',
+  templateUrl: './aide.component.html',
+  styleUrls: ['./aide.component.css']
+})
+export class AideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { HeaderComponent } from './header/header.component';
 import { SlideComponent } from './slide/slide.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '../material/material.module';
+import { AideComponent } from './aide/aide.component';
+import { AproposComponent } from './apropos/apropos.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +29,9 @@ import { MaterialModule } from '../material/material.module';
     FooterComponent,
     HeaderComponent,
     SlideComponent,
-    BlogComponent
+    BlogComponent,
+    AideComponent,
+    AproposComponent
   ],
   imports: [
     BrowserModule,
