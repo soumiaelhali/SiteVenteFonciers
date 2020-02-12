cff4f2ee7b1d544256bf930f72f2fa1910945b7e
EttouibiaSalma <ettoui.salma@gmail.com>
2020-02-12 17:14:18 +0100

    updates

--- src/app/authentification/authentification.component.ts
+++ src/app/authentification/authentification.component.ts
@@ -12,35 +12,14 @@ import { ok } from 'assert';
   templateUrl: './authentification.component.html',
   styleUrls: ['./authentification.component.css']
 })
-export class AuthentificationComponent implements OnInit,HttpInterceptor {
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const { url, method, headers, body } = request;
-  }
-  comptes = [];
-  private compte: Compte = {
-    FirstName: '',   
-    LastName: '',
-    email: '',  
-    password:'',
-    mobile: '',
-    gender: ''
-};
+export class AuthentificationComponent implements OnInit {
+  
+  
 
-  constructor(private service: CompteService, private router: Router) { }
+  constructor() { }
 
   ngOnInit() {
   } 
-  authenticate() {
-    const { email, password } = body;
-    const compte = comptes.find(x => x.email === email && x.password === password);
-    if (!compte) return error('email or password is incorrect');
-    return ok({
-        id: compte.id,
-        email: compte.email,
-        firstName: compte.firstName,
-        lastName: compte.lastName,
-        token: 'fake-jwt-token'
-    })
-}
+  
 
 }
